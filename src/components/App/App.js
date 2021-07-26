c92b3e0a3a104ad305e153f475d5dafc065b46c8
Ржанов Володимир <emitter@10a>
2021-07-26 17:00:36 +0300

    переделал на if

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,8 @@ export default class App extends Component {
     page: 1,
     loading: false,
     modalShow: false,
-    error: ''
+    error: '',
+    status: 'idle'
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -34,17 +35,16 @@ export default class App extends Component {
   };
 
   fetchImages = (query, page) => {
-    this.setState({ loading: true });
+    this.setState({ status:'pending' });
     imageApi
       .fetchImg(query, page)
       .then(({ data: { hits } }) => {
         this.setState((prevState) => ({
-          images: [...prevState.images, ...mapper(hits)],
+          images: [...prevState.images, ...mapper(hits)], status: 'resolved'
         }));
       }
       )
-      .catch((error) => this.setState({error}))
-      .finally(() => this.setState({ loading: false }));
+      .catch((error) => this.setState({ error, status: 'rejected' }))
   };
 
   reset = () => {
@@ -68,47 +68,176 @@ export default class App extends Component {
     this.setState(({ modalShow }) => ({ modalShow: !modalShow, modalImg, tags }));
   };
 
+
+
+// ------------------------------------------------ Вариант 1 ------------------------------------------------------
+  
+                                              //  Сделал три варианта ( надеюсь правильно))) ). Как лучше/правильно ?
+  
+  
   render() {
-    const { images, loading, modalShow, modalImg, tags, error } = this.state;
+    const { images, status, modalShow, modalImg, tags, error } = this.state;
     return (
       <>
         <SearchBar
           onSubmit={this.handleSubmit}
         />
         <Section>
+          {(status === 'pending') &&
+            <>
+              <ImageGallery
+                images={images}
+                onModalOpen={this.toggleModal}
+              />
+              <Loader />
+            </>
+          }
+
           {
-            error &&
+            (status === "rejected") &&
             <h2>
               {error.message}
             </h2>
           }
-          {
-            !!images.length &&
-            <ImageGallery
-              images={images}
-              onModalOpen={this.toggleModal}
-            />
-          }
-          {
-            loading &&
-            <Loader />
-          }
-          {
-            (images.length % 12===0 && !!images.length) &&
-            <Button
-              onLoadMore={this.handleNextPage}
-            />
+
+          {(status === 'resolved') &&
+            <>
+              <ImageGallery
+                images={images}
+                onModalOpen={this.toggleModal}
+              />
+              {(images.length % 12 === 0) &&
+                <Button
+                  onLoadMore={this.handleNextPage}
+                />
+              }
+              {
+                modalShow &&
+                <Modal
+                  onClose={this.toggleModal}
+                  modalImg={modalImg}
+                  tags={tags}
+                />
+              }
+            </>
           }
         </Section>
-        {
-          modalShow &&
-          <Modal
-            onClose={this.toggleModal}
-            modalImg={modalImg}
-            tags={tags}
-          />
-        }
       </>
-    );
+    )
   };
+
+
+// ------------------------------------------Вариант 2----------------------------------------------------
+
+  // render() {
+  //   const { images, status, modalShow, modalImg, tags, error } = this.state;
+  //   if (this.state.status === 'idle') {
+  //     return <>
+  //       <SearchBar
+  //         onSubmit={this.handleSubmit}
+  //       />;
+  //       <Section />
+  //     </>
+  //   }
+
+  //   if (status === 'pending') {
+  //     return <>
+  //       <SearchBar
+  //         onSubmit={this.handleSubmit}
+  //       />;
+  //       <Section>
+  //         <ImageGallery
+  //           images={images}
+  //           onModalOpen={this.toggleModal}
+  //         />
+  //         <Loader />
+  //       </Section>
+  //     </>
+  //   }
+
+  //   if (status === 'resolved') {
+  //     return <>
+  //       <SearchBar
+  //         onSubmit={this.handleSubmit}
+  //       />;
+  //       <Section>
+  //         <ImageGallery
+  //           images={images}
+  //           onModalOpen={this.toggleModal}
+  //         />
+  //         {(images.length % 12 === 0) &&
+  //           <Button
+  //             onLoadMore={this.handleNextPage}
+  //           />}
+  //       </Section>
+  //       {
+  //         modalShow &&
+  //         <Modal
+  //           onClose={this.toggleModal}
+  //           modalImg={modalImg}
+  //           tags={tags}
+  //         />
+  //       }
+  //     </>
+  //   }
+
+  //   if (status === "rejected") {
+  //     return <>
+  //       <SearchBar
+  //         onSubmit={this.handleSubmit}
+  //       />
+  //       <Section>
+  //         <h2>{error.message}</h2>
+  //       </Section>
+  //     </>
+  //   }
+  // }
+    
+
+// ------------------------------ Вариант 3-------------------------------------------
+
+// render() {
+    // const { images, loading, modalShow, modalImg, tags, error } = this.state;
+
+    // return (
+    //   <>
+    //     <SearchBar
+    //       onSubmit={this.handleSubmit}
+    //     />
+    //     <Section>
+    //       {
+    //         error &&
+    //         <h2>
+    //           {error.message}
+    //         </h2>
+    //       }
+    //       {
+    //         !!images.length &&
+    //         <ImageGallery
+    //           images={images}
+    //           onModalOpen={this.toggleModal}
+    //         />
+    //       }
+    //       {
+    //         loading &&
+    //         <Loader />
+    //       }
+    //       {
+    //         (images.length % 12===0 && !!images.length) &&
+    //         <Button
+    //           onLoadMore={this.handleNextPage}
+    //         />
+    //       }
+    //     </Section>
+    //     {
+    //       modalShow &&
+    //       <Modal
+    //         onClose={this.toggleModal}
+    //         modalImg={modalImg}
+    //         tags={tags}
+    //       />
+    //     }
+    //   </>
+    // );
+  // };
 };
